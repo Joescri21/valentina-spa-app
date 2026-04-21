f7d1d069dbc8b1a08f39bd55021fff1e6de6ad52
Joel Escribano Bonola <joescri21@gmail.com>
2026-04-20 23:49:09 -0600

    Add. cambio en la ruta y se crea home, que va se un hero

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,28 +3,30 @@ import { LandingPage } from './public/landing-page/landing-page';
 import { LoginPage } from './auth/login-page/login-page';
 import { DashboardPage } from './client/dashboard-page/dashboard-page';
 import { PosPage } from './admin/pos-page/pos-page';
+import { Home } from './public/components/home/home';
+import { Catalogo } from './public/components/catalogo/catalogo';
 
 export const routes: Routes = [
   {
     path: 'home',
     component: LandingPage,
-    title: 'Valentina Spa'
-
+    title: 'Valentina Spa',
+    children: [
+      {
+        path: '',
+        component: Home,
+      },
+      {
+        path: 'catalgo',
+        component: Catalogo,
+        title: 'Catálogo de Productos',
+      }
+    ]
   },
   {
     path: 'login',
     component: LoginPage,
-    title: 'Login - Darshbord'
-  },
-  {
-    path: 'Client Dashboard',
-    component: DashboardPage,
-    title: 'Client Dashboard'
-  },
-  {
-    path: 'Admin Dashboard',
-    component: PosPage,
-    title: 'Admin Dashboard'
+    title: 'Iniciar Sesión',
   },
   {
     path: '**',
--- src/app/public/landing-page/landing-page.ts
+++ src/app/public/landing-page/landing-page.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { Navbar } from "../components/navbar/navbar";
+import { Home } from "../components/home/home";
 
 @Component({
   selector: 'landing-page',
-  imports: [Navbar],
+  imports: [
+    Navbar,
+    Home
+  ],
   templateUrl: './landing-page.html',
 })
 export class LandingPage { }
