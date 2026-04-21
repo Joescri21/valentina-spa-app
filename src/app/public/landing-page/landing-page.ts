8d980ccb069c1e54a6af60696a58ea9f52043167
Joel Escribano Bonola <joescri21@gmail.com>
2026-04-21 07:36:34 -0600

    Add: Cambio en el HTMl

--- src/app/public/landing-page/landing-page.ts
+++ src/app/public/landing-page/landing-page.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { Navbar } from "../components/navbar/navbar";
 import { Home } from "../components/home/home";
+import { Promociones } from "../components/promociones/promociones";
+import { Catalogo } from "../components/catalogo/catalogo";
 
 @Component({
   selector: 'landing-page',
   imports: [
     Navbar,
-    Home
-  ],
+    Home,
+    Promociones,
+    Catalogo
+],
   templateUrl: './landing-page.html',
 })
 export class LandingPage { }
