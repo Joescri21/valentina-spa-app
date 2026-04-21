4ca9de0874b2e6889bc8c6a07a3cc8b53ac0ac91
Joel Escribano Bonola <joescri21@gmail.com>
2026-04-20 23:33:40 -0600

    Add: Mejor en el Navbar

--- src/app/public/components/navbar/navbar.ts
+++ src/app/public/components/navbar/navbar.ts
@@ -14,54 +14,7 @@ import { Component } from '@angular/core';
     padding: 1rem;
     margin: 0 auto;
   }
-  section {
-    width: 50%;
-    display: flex;
-    gap: 1rem;
-  }
-  .logo{
-    font-family: "Coiny", system-ui;
-    font-weight: 400;
-    font-style: normal;
-    font-size: 2rem;
-  }
-  .nav__menu{
-    display: flex;
-    gap: 1rem;
-    justify-content: space-between;
-    align-items: center;
-  }
-  .nav__menu--item{
-    text-decoration: none;
-    color: #000;
-    font-size: 1.3rem;
-    font-weight: bold;
-  }
-  .nav__menu--item:hover{
-    color: #E91E63;
-    font-size: 1.3rem;
-  }
-  .nav__menu--boton{
-    background-image: linear-gradient(to right, #E91E63 0%, #B21F83 51%, #7B1FA2 100%);
 
-    margin: 10px;
-    padding: 15px 45px;
-    text-align: center;
-    text-transform: uppercase;
-    transition: 0.5s;
-    background-size: 200% auto;
-    color: white;
-    box-shadow: 0 0 20px #eee;
-    border-radius: 10px;
-    display: block;
-    outline: none;
-    border: none;
-  }
-  .nav__menu--boton:hover{
-    background-position: right center;
-    color: #fff;
-    text-decoration: none;
-  }
   `
 })
 export class Navbar { }
