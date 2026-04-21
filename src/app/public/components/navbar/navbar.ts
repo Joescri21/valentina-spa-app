37050cabbb76de1200f84bab6581971d7863ee59
Joel Escribano Bonola <joescri21@gmail.com>
2026-04-21 07:38:44 -0600

    Add: Cambio en el Nav color

--- src/app/public/components/navbar/navbar.ts
+++ src/app/public/components/navbar/navbar.ts
@@ -4,17 +4,5 @@ import { Component } from '@angular/core';
   selector: 'app-navbar',
   imports: [],
   templateUrl: './navbar.html',
-
-  styles: `
-  .navbar {
-    background-color: #fffff;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 1rem;
-    margin: 0 auto;
-  }
-
-  `
 })
 export class Navbar { }
