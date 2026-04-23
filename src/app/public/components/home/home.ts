467bb186201071efaeb72493d826be6b6d20aaa2
Joel Escribano Bonola <joescri21@gmail.com>
2026-04-23 11:39:40 -0600

    Add: carriuselmovil

--- src/app/public/components/home/home.ts
+++ src/app/public/components/home/home.ts
@@ -5,4 +5,44 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './home.html',
 })
-export class Home { }
+export class Home {
+
+  currentSlide = 1;
+  totalSlides = 4; // Ajusta según cuántas fotos tengas
+  timer: any;
+
+  ngOnInit() {
+    this.startAutoPlay();
+  }
+
+  startAutoPlay() {
+    this.timer = setInterval(() => {
+      this.nextSlide();
+    }, 4000); // Cambia cada 4 segundos
+  }
+
+  nextSlide() {
+    this.currentSlide++;
+    if (this.currentSlide > this.totalSlides) {
+      this.currentSlide = 1;
+    }
+
+    // Buscamos el elemento por ID y lo scrolleamos
+    const element = document.getElementById(`item${this.currentSlide}`);
+    if (element) {
+      element.scrollIntoView({
+        behavior: 'smooth',
+        block: 'nearest',
+        inline: 'start'
+      });
+    }
+  }
+
+  ngOnDestroy() {
+    // Muy importante limpiar el timer al salir
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
+  }
+
+}
